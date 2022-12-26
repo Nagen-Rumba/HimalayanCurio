9f978f92cbc296afb7c75e632ba0758b7134825f
gundruk09 <52064304+gundruk09@users.noreply.github.com>
2022-12-26 11:04:56 +0700

    new

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -101,7 +101,7 @@ const Cart = () => {
             </div>
             <div className="btn-container">
               <button type="button" className="btn" onClick={handleCheckout}>
-                Pay with Stripessssss
+                Pay with Stripe
               </button>
             </div>
           </div>
