9b01a37b24f1acb7989e6788b54e940d29d9a176
gundruk09 <52064304+gundruk09@users.noreply.github.com>
2022-12-26 10:56:06 +0700

    Test

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -101,7 +101,7 @@ const Cart = () => {
             </div>
             <div className="btn-container">
               <button type="button" className="btn" onClick={handleCheckout}>
-                Pay with Stripe
+                Pay with Stripessssss
               </button>
             </div>
           </div>
